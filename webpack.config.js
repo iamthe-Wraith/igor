df9e4b8cfa01cb582f7187d6e8705d83d3c53fe7
iamtheWraith <allhailthewraith@protonmail.com>
2020-06-09 21:41:10 -0400

    added webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const { resolve } = require('path');
+const { BannerPlugin } = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  mode: 'none',
+  output: {
+    path: resolve(__dirname, 'bin'),
+    filename: 'igor'
+  },
+  target: 'node',
+  entry: ['@babel/polyfill', './src/index.js'],
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new BannerPlugin({
+      banner: '#!/usr/bin/env node\n',
+      raw: true
+    })
+  ]
+};
